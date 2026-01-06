f916235a7c37d2d06755a0b2d1aafe2e638029da
franpavlovi <fljubuski@gmail.com>
2026-01-07 00:10:10 +0100

    tailwind setup

--- CalorixFrontend/README.md
+++ CalorixFrontend/README.md
@@ -1,4 +1,4 @@
-# Calorix - Frontend
+# CalorixFrontend
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ CalorixFrontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+
+  <div class="min-h-screen bg-black flex items-center justify-center">
+    <h1 class="text-5xl font-extrabold text-pink-500">
+      Tailwind radi ✅
+    </h1>
+  </div>
+</template>
+
+<style scoped></style>
